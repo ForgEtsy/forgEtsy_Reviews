b06fec88f323109ce6888f0c926078323e96692f
Jonathan Keane <jkeane889@gmail.com>
2019-10-09 07:18:20 -0500

    Created initial reviews repo with Master proxy client, server and database file structure

---
+++ server/server.js
@@ -0,0 +1,12 @@
+const express = require('express')
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+let port = 3003;
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}...`);
+})
